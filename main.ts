3228a8b2476e7c11ca487362eb7fac860c48e926
JacquelineOchoa <110990951+JacquelineOchoa@users.noreply.github.com>
2022-10-13 07:40:17 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showIcon(IconNames.No)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.SmallHeart)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showIcon(IconNames.Tortoise)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showIcon(IconNames.Butterfly)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showIcon(IconNames.Fabulous)
+})
